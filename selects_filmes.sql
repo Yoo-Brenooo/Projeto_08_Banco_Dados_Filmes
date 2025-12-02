7690eb01a078edc5beffd5f294d5ee3fa6393146
Breno <brenooliveirasouza13@gmail.com>
2025-12-02 15:05:16 -0300

    Add files via upload

--- selects_filmes.sql
+++ selects_filmes.sql
@@ -37,27 +37,35 @@ ORDER BY duracao_minutos DESC
 LIMIT 10;
 
 -- 6. Eu quero saber quais são os filmes do Diretor Christopher Nolan
-
+select * from Filmes where diretor = "Christopher Nolan"
 -- 7. Eu quero saber quais são os filmes do James Gunn
-
+select * from Filmes where diretor = "James Gunn"
 -- 8. Eu quero saber quais os filmes de ação lançados em 2016
-
+select * from Filmes where ano_lancamento = "2016"
 -- 9. Eu quero saber quais os filmes do batman e quais os diretores
+select * from Filmes where titulo like "%Batman%"
 
 -- 10. Eu quero saber quais os filmes de terror e drama lançado entre 1980 e 2002
-
+select * from filmes where genero in ("Terror", "Drama") and ano_lancamento BETWEEN 1980 and 2002 order by ano_lancamento
 -- 11. Eu quero Saber quantos filmes foram lançados em 1999
+select * from Filmes where ano_lancamento = 1999
 
 -- 12. Eu quero saber quantos filmes tem o diretor Quentin Tarantino
+select count(titulo) as total_filmes from Filmes where diretor = "Quentin Tarantino"
 
 -- 13. Eu quero saber quais os filmes do James Cameron e quais os generos deles
+select titulo,genero from Filmes where diretor = "James Cameron"
 
 -- 14. Por fim vamos adiconar as informações do seu filme favorito na tabela.
+insert into filmes (titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb) values ("Prenda-Me se For Capaz", "Crime", 2002, 141, "Steven Spielberg", 8.2)
 
 -- 15. Vamos adiconar as informações do filme que o professor odeia.
+insert into filmes (titulo, genero, ano_lancamento, duracao_minutos, diretor, nota_imdb) values ("Homem-Aranha: Longe de Casa", "Ficção Científica", 2019, 130, "Jon Watts", 7.3)
 
 -- 16. Vamos deletar esse filme que o professor detesta.
+delete from Filmes where titulo = "Homem-Aranha: Longe de Casa"
 
+SELECT * FROM filmes
 
 
 
